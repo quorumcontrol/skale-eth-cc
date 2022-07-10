7db10b30b5f86585788ff7375ea8507ef5221b03
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-07-10 10:17:25 -0700

    BTl2 - Commitment Tracker, Contract Styling, Record Tracking with Total Battles

--- contracts/contracts/Battle.sol
+++ contracts/contracts/Battle.sol
@@ -17,11 +17,16 @@ import "./interfaces/IGameItems.sol";
 **/
 contract Battle {
 
-  /// @notice The Game Items Contract Address
-  address immutable gameItemAddress;
+  ////////////////////////////////////////
+  //////////////// State /////////////////
+  ////////////////////////////////////////
 
+  /// @notice The Game Items Contract Address
+  address immutable private gameItemAddress;
   /// @notice The committed list
   mapping(address => bytes32) private committed;
+  /// @notice Number of Times Commited
+  mapping(address => uint256) private timesCommitted;
   /// @notice Win List
   mapping(address => uint256) private wins;
   /// @notice Loss List
@@ -29,14 +34,22 @@ contract Battle {
   /// @notice Draw List
   mapping(address => uint256) private draws;
 
+  ////////////////////////////////////////
+  //////////////// Errors ////////////////
+  ////////////////////////////////////////
+
   /// @notice Commit Already Exists Error
   string private constant COMMIT_ALREADY_EXISTS = "Commit Already Exists";
   /// @notice Commit Emtpy Cannot Battle
   string private constant CANNOT_BATTLE = "Comimt Empty: Battle Impossible";
 
+  ////////////////////////////////////////
+  //////////////// Events ////////////////
+  ////////////////////////////////////////
+
   /// @notice Emits the End of a Battle
   event BattleCompleted(address indexed playerOne, address indexed playerTwo, uint8 result, uint256 playerOneItem, uint256 playerTwoItem);
-
+  event Commited(address indexed committer, bytes32 indexed commitment, uint256 commitmentNumber);
   /// @dev Sets Game Item Contract to the active Game Items
   /// @dev Script MUST add WIN_MANAGER_ROLE to THIS
   /// @param _gameItemAddress Address of Game Item
@@ -108,6 +121,8 @@ contract Battle {
   function commitItem(bytes32 commitment) external {
     require(_canCommit(), COMMIT_ALREADY_EXISTS);
     committed[msg.sender] = commitment;
+    timesCommitted[msg.sender] = timesCommitted[msg.sender]++;
+    emit Commited(msg.sender, commitment, timesCommitted[msg.sender]);
   }
 
   /// @dev Gets Address of The Game Item Contract
@@ -116,7 +131,15 @@ contract Battle {
     return gameItemAddress;
   }
 
-  
+  /// @dev Retrieves Record for a player
+  /// @dev Returns the record in the format Wins-Losses-Draws-BATTLES with each value being uint256 due to mappipng setup
+  /// @notice Depending on our thoughts here, although on SKALE it does not matter, we could look to lower the maximum win-loss-draw size as the uint256 size may be much larger than required
+  /// @param player the address of the player that the record is for
+  /// @return [uint256, uint256, uint256, uint256] WINS-LOSSES-DRAWS-BATTLES
+  function getRecord(address player) external view returns (uint256, uint256, uint256, uint256) {
+    return (wins[player], losses[player], draws[player], wins[player] + losses[player] + draws[player]);
+  }
+
   ////////////////////////////////////////
   /////////////// Internal ///////////////
   ////////////////////////////////////////
@@ -141,10 +164,13 @@ contract Battle {
   }
 
   /// @dev Finds Winner :) 
-  /// @return bool 0 if P1 wins, 1 if P2 Wins, 3 if Draw
+  /// @param itemsContract the Game Items Proxy Contract
+  /// @param p1TokenId the Token Id Player 1 Commited
+  /// @param p2TokenId the tokenId Player 2 Commited
+  /// @return bool 0 if P1 wins, 1 if P2 Wins, 2 if Draw
   function _findWinner(IGameItems itemsContract, uint256 p1TokenId, uint256 p2TokenId) internal returns (uint8) {
     /// If Match -> Automatic Draw
-    if (p1TokenId == p2TokenId) return 3;
+    if (p1TokenId == p2TokenId) return 2;
     /// Check P1 Beats
     IGameItems.GameItemMetadata memory p1Item = itemsContract.getOnChainToken(p1TokenId);
     for (uint256 i = 0; i < p1Item.beats.length; i++) {
@@ -160,11 +186,15 @@ contract Battle {
       }
     }
     /// Automatic Return Draw Else
-    return 3;
+    return 2;
 
 
   }
 
+  /// @dev Sets the Results for a Player After a Successfull Battle
+  /// @param p1 Address of Player 1
+  /// @param p2 Address of Player 2
+  /// @param result The Result of the battle where 0 = p1 win, 1 = p2 win, 2 = draw
   function _setResult(address p1, address p2, uint8 result) internal {
     if (result == 0) {
       wins[p1] = wins[p1]++;
