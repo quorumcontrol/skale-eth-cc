c795c08d17c9a554a8f8467bac8c88080549099f
Topper Bowers <topper@toppingdesign.com>
2022-07-11 16:24:20 +0200

    only client side

--- pages/inventory.tsx
+++ pages/inventory.tsx
@@ -1,19 +1,21 @@
 import { Text } from "@chakra-ui/react";
 import NFTCard from "../src/components/NFTCard";
 import { useInventory } from "../src/hooks/useGameItems";
+import useIsClientSide from "../src/hooks/useIsClientSide";
 import Layout from "../src/layouts/Layout";
 
 export default function Inventory() {
   const { data: tokens } = useInventory()
+  const isClient = useIsClientSide()
 
   return (
     <Layout>
       <Text>Inventory</Text>
-      {tokens && tokens.length == 0 && (
+      {isClient && tokens && tokens.length == 0 && (
         <Text>Looks like you have no items, you'll need to get an invite code.</Text>
       )}
       {
-        tokens && tokens.map((token) => {
+        isClient && tokens && tokens.map((token) => {
           return <NFTCard item={token} key={`inventory-card-${token.tokenId}`}/>
         })
       }
