f88eb1e02bac482f6a7be994d40733e1f080cf59
Topper Bowers <topper@toppingdesign.com>
2022-07-14 23:07:23 +0200

    dynamic require so that we do not run intoo a missing localaddresses

--- src/utils/networkSelector.ts
+++ src/utils/networkSelector.ts
@@ -1,4 +1,3 @@
-import localAddresses from '../../contracts/deployments/localhost/addresses.json'
 import { localhost, skaleTestnet } from './SkaleChains'
 
 export enum chainEnvs {
@@ -12,7 +11,9 @@ export const activeChain = process.env.NEXT_PUBLIC_CHAIN || chainEnvs.local
 export function addresses() {
   switch (activeChain) {
     case chainEnvs.local:
-      return localAddresses
+      return require('../../contracts/deployments/localhost/addresses.json')
+    case chainEnvs.test:
+      return require('../../contracts/deployments/skaletest/addresses.json')
     default:
       throw new Error("UI doesn't yet support that chain")
   }
