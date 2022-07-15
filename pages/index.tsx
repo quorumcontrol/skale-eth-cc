d6392873b3efa3d7fc8cebd7e02758e9c8cbae2a
Topper Bowers <topper@toppingdesign.com>
2022-07-15 22:34:16 +0200

    add video to home screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,22 @@
-import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Spinner, Text } from "@chakra-ui/react";
 import { useAccount } from "wagmi";
-import NextLink from 'next/link'
+import NextLink from "next/link";
 import { useInventory } from "../src/hooks/useGameItems";
 import useIsClientSide from "../src/hooks/useIsClientSide";
 import Layout from "../src/layouts/Layout";
+import Video from "../src/components/Video";
 
 export default function Home() {
-  const { address, isConnected } = useAccount()
-  const { data:tokens, isFetching} = useInventory()
-  const isClient = useIsClientSide()
+  const { address, isConnected } = useAccount();
+  const { data: tokens, isFetching } = useInventory();
+  const isClient = useIsClientSide();
 
   if (!isClient || !isConnected) {
     return (
       <Layout>
-        <Text>
-          Connect your wallet to begin
-        </Text>
+        <Text>Connect your wallet to begin</Text>
       </Layout>
-    )
+    );
   }
 
   if (isFetching) {
@@ -25,26 +24,28 @@ export default function Home() {
       <Layout>
         <Spinner />
       </Layout>
-    )
+    );
   }
 
   return (
     <Layout>
-      <Heading>Welcome to Skale, Paper, Scissors</Heading>
-      <Text>A classic game, reimagined on the zero-gas SKALE network </Text>
-      <Text>Find someone to play against, choose an item, scan a QR code.</Text>
-      <Text>If you win, you get their item.</Text>
-      <Text>Collect all the items to win $2022 in prizes.</Text>
-      { isClient && address && tokens?.length === 0 && (
+      {isClient && address && tokens?.length === 0 && (
         <NextLink href="/signup">
           <Button>Signup</Button>
         </NextLink>
       )}
-      { isClient && address && (tokens?.length || 0) > 0 && (
+      {isClient && address && (tokens?.length || 0) > 0 && (
         <NextLink href="/inventory">
           <Button>Play</Button>
         </NextLink>
       )}
+      <Box>
+        <Video animationUrl="/promoVid.mp4" autoPlay controls></Video>
+      </Box>
+      <Text>A classic game, reimagined on the zero-gas SKALE network </Text>
+      <Text>Find someone to play against, choose an item, scan a QR code.</Text>
+      <Text>If you win, you get their item.</Text>
+      <Text>Collect all the items to win $2022 in prizes.</Text>
     </Layout>
-  )
+  );
 }
