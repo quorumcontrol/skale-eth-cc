3a10f691c88d5a44a8ca4b7357ff8cfe4888ee2c
Topper Bowers <topper@toppingdesign.com>
2022-07-16 21:53:30 +0200

    remove comments

--- src/browserWallet/torusConnector.ts
+++ src/browserWallet/torusConnector.ts
@@ -12,20 +12,8 @@ import {
 import { getTorus, torus } from './torus'
 
 type InjectedConnectorOptions = {
-  /** Name of connector */
   name?: string | ((detectedName: string | string[]) => string)
-  /**
-   * MetaMask 10.9.3 emits disconnect event when chain is changed.
-   * This flag prevents the `"disconnect"` event from being emitted upon switching chains.
-   * @see https://github.com/MetaMask/metamask-extension/issues/13375#issuecomment-1027663334
-   */
   shimChainChangedDisconnect?: boolean
-  /**
-   * MetaMask and other injected providers do not support programmatic disconnect.
-   * This flag simulates the disconnect behavior by keeping track of connection status in storage.
-   * @see https://github.com/MetaMask/metamask-extension/issues/10353
-   * @default true
-   */
   shimDisconnect?: boolean
 }
 
@@ -33,9 +21,6 @@ export type MetaMaskConnectorOptions = Pick<
   InjectedConnectorOptions,
   'shimChainChangedDisconnect' | 'shimDisconnect'
 > & {
-  /**
-   * While "disconnected" with `shimDisconnect`, allows user to select a different MetaMask account (than the currently connected account) when trying to connect.
-   */
   UNSTABLE_shimOnConnectSelectAccount?: boolean
 }
 
@@ -44,7 +29,6 @@ export class TorusConnector extends InjectedConnector {
   readonly ready = true
 
   provider?: ethers.providers.ExternalProvider
-  // #UNSTABLE_shimOnConnectSelectAccount: MetaMaskConnectorOptions['UNSTABLE_shimOnConnectSelectAccount']
 
   constructor({
     chains,
@@ -63,9 +47,6 @@ export class TorusConnector extends InjectedConnector {
       chains,
       options,
     })
-
-    // this.#UNSTABLE_shimOnConnectSelectAccount =
-    //   options.UNSTABLE_shimOnConnectSelectAccount
   }
 
   async connect({ chainId }: { chainId?: number } = {}) {
@@ -91,26 +72,6 @@ export class TorusConnector extends InjectedConnector {
 
       this.emit('message', { type: 'connecting' })
 
-      // Attempt to show wallet select prompt with `wallet_requestPermissions` when
-      // `shimDisconnect` is active and account is in disconnected state (flag in storage)
-      // if (
-      //   // this.#UNSTABLE_shimOnConnectSelectAccount &&
-      //   this.options?.shimDisconnect &&
-      //   // !getClient().storage?.getItem(shimDisconnectKey)
-      // ) {
-      //   const accounts = await provider
-      //     .request({
-      //       method: 'eth_accounts',
-      //     })
-      //     .catch(() => [])
-      //   const isConnected = !!accounts[0]
-      //   if (isConnected)
-      //     await provider.request({
-      //       method: 'wallet_requestPermissions',
-      //       params: [{ eth_accounts: {} }],
-      //     })
-      // }
-
       const account = await this.getAccount()
       // Switch to chain if provided
       let id = await this.getChainId()
@@ -120,9 +81,6 @@ export class TorusConnector extends InjectedConnector {
         id = chain.id
         unsupported = this.isChainUnsupported(id)
       }
-
-      // if (this.options?.shimDisconnect)
-      //   getClient().storage?.setItem(shimDisconnectKey, true)
 
       return { account, chain: { id, unsupported }, provider }
     } catch (error) {
@@ -138,20 +96,4 @@ export class TorusConnector extends InjectedConnector {
    this.provider = (await getTorus()).provider
    return this.provider as Ethereum
   }
-
-  // private getReady(ethereum?: Ethereum) {
-  //   const isMetaMask = !!ethereum?.isMetaMask
-  //   if (!isMetaMask) return
-  //   // Brave tries to make itself look like MetaMask
-  //   // Could also try RPC `web3_clientVersion` if following is unreliable
-  //   if (ethereum.isBraveWallet && !ethereum._events && !ethereum._state) return
-  //   if (ethereum.isTokenPocket) return
-  //   if (ethereum.isTokenary) return
-  //   return ethereum
-  // }
-
-  // private findProvider(ethereum?: Ethereum) {
-  //   if (ethereum?.providers) return ethereum.providers.find(this.#getReady)
-  //   return this.#getReady(ethereum)
-  // }
 }
