d3e900a727db8b1f0042ab1b67fd158a0b1454fb
Topper Bowers <topper@toppingdesign.com>
2022-07-18 11:30:42 +0200

    make the about page at least not broken

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import { Heading, Text } from "@chakra-ui/react"
+import { Heading, Text, Box, VStack } from "@chakra-ui/react"
 import AppLink from "../src/components/AppLink"
 import Layout from "../src/layouts/Layout"
 
@@ -6,13 +6,26 @@ const About:React.FC = () => {
   return (
     <Layout>
       <Heading>Welcome to the #skaleverse</Heading>
-      <Text>Skale, Paper, Scissors is lovingly created by a group of product builders on the SKALE network.</Text>
-      {/* TODO: write up on the game? */}
+      <Text>SKALE, Paper, Scissors is lovingly created by a group of product builders on the <AppLink href="https://skale.space/">SKALE network.</AppLink></Text>
       <Heading>Credits</Heading>
+      <Text>Game design: Andrew Holz</Text>
       <Text>Coding: <AppLink href="https://github.com/thegreataxios">TheGreatAxios</AppLink> and <AppLink href="https://github.com/tobowers">Topper Bowers</AppLink></Text>
-      {/* TODO: links here*/}
-      <Text>Product, design, marketing support from: Ruby.exchange, Mylilius, LarvaMaiorum, SKALE.</Text>
-      <Text>3D Models: {/* TODO: drop in the 3d credits here*/}</Text>
+      <Text>Product, design, marketing support from:{" "}
+        <AppLink href="https://ruby.exchange/">Ruby.exchange</AppLink>,{" "}
+        <AppLink href="https://www.mylilius.com/">Mylilius</AppLink>,{" "}
+        <AppLink href="https://larvamaiorum.com/">LarvaMaiorum</AppLink>,{" "}
+        <AppLink href="https://skale.space/">SKALE.</AppLink></Text>
+      <Text>3D Models:</Text>
+      <VStack>
+        <Text>Rock: <AppLink href="https://sketchfab.com/SaschaHenrichs">SasschaHenrichs</AppLink></Text>
+        <Text>scissors: <AppLink href="https://sketchfab.com/tudor_macovei">tudor_macovei</AppLink></Text>
+        <Text>Old Map: <AppLink href="https://sketchfab.com/Johana-PS">Johana-PS</AppLink></Text>
+        <Text>Lizard: <AppLink href="https://sketchfab.com/DigitalLife3D">DigitalLife3D</AppLink></Text>
+        <Text>Mirror: <AppLink href="https://sketchfab.com/AllyC3D">AllyC3D</AppLink></Text>
+        <Text>Torch: <AppLink href="https://sketchfab.com/milacetious">milacetious</AppLink></Text>
+        <Text>Pepper: <AppLink href="https://sketchfab.com/animator12">animator12</AppLink></Text>
+        <Text>Polar Bear: <AppLink href="https://sketchfab.com/turunmuseokeskus">turunmuseokeskus</AppLink></Text>
+      </VStack>
     </Layout>
   )
 }
