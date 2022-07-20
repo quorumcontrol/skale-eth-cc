a97c84b658ca84ed4a786394b4221d2b32114f11
Topper Bowers <topper@toppingdesign.com>
2022-07-20 20:55:54 +0200

    tasks

--- contracts/tasks/helpers.ts
+++ contracts/tasks/helpers.ts
@@ -14,3 +14,18 @@ export async function getGameItemsContract(
 
   return GameItems__factory.connect(deploy.address, signer);
 }
+
+export async function getBatleContract(
+  hre: HardhatRuntimeEnvironment
+) {
+  console.log("using network: ", hre.network.name);
+  const deploy = await import(
+    `../deployments/${hre.network.name}/Battle.json`
+  );
+
+  const { Battle__factory } = await import("../typechain-types");
+
+  const signer = (await hre.ethers.getSigners())[0];
+
+  return Battle__factory.connect(deploy.address, signer);
+}
